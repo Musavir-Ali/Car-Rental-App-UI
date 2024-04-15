324ebd7ab034a70ca09da650e9fbf55fa6b744ed
Musavir <88766824+Musavir-Ali@users.noreply.github.com>
2024-04-16 02:40:50 +0500

    Update

--- lib/Splash.dart
+++ lib/Splash.dart
@@ -1,7 +1,4 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -14,9 +11,9 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          Colors.transparent, // Set background color to transparent
+      backgroundColor: Colors.transparent,
       body: Container(
+        width: double.infinity,
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/Image.png'),
@@ -25,151 +22,15 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
         child: Column(
           children: [
-            // SizedBox(
-            //   width: MediaQuery.of(context).size.width,
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.center,
-            //     children: [
-            //       Image.asset(
-            //         'assets/images/Logo (1).png',
-            //         height: 100,
-            //         width: 100,
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.4),
             SizedBox(
-              height: 100.0,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    " ",
-                    style: TextStyle(
-                      fontSize: 43.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10.0,
-                  ),
-                  DefaultTextStyle(
-                    style: const TextStyle(
-                      fontSize: 0.0,
-                      color: Colors.white,
-                    ),
-                    child: AnimatedTextKit(
-                      repeatForever: true,
-                      animatedTexts: [
-                        RotateAnimatedText(
-                          'TESLA',
-                          textStyle: GoogleFonts.anton(
-                            fontSize: 43.0,
-                            color: Colors.redAccent,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        RotateAnimatedText(
-                          'MERCEDES',
-                          textStyle: GoogleFonts.anton(
-                            fontSize: 43.0,
-                            color: Colors.redAccent,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        RotateAnimatedText(
-                          'BMW',
-                          textStyle: GoogleFonts.anton(
-                            fontSize: 43.0,
-                            color: Colors.redAccent,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        RotateAnimatedText(
-                          'AUDI',
-                          textStyle: GoogleFonts.anton(
-                            fontSize: 43.0,
-                            color: Colors.redAccent,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                      // onTap: () {
-                      //   print("Tap Event");
-                      // },
-                    ),
-                  ),
-                ],
-              ),
+              height: MediaQuery.of(context).size.height * 0.7,
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Ride In Style",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  "Order a luxury ride immediately,",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 12.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  "for book a car for upcoming event.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 12.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "Get Started",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.redAccent),
-                ),
+            const Text(
+              "RIDE IN STYLE",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ],
