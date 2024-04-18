3e5abad460194dc6bb0e03cd62e962e3bfe16752
Musavir <88766824+Musavir-Ali@users.noreply.github.com>
2024-04-19 03:01:11 +0500

    update

--- lib/Splash.dart
+++ lib/Splash.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/widgets.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,66 +11,86 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  int _currentIndex = 0;
+
+// Define list of texts corresponding to each image
+  final List<String> imageTexts = [
+    'ORDER A RIDE ',
+    'SELECT A CAR',
+    'RIDE IN STYLE',
+  ];
+  final List<String> secondTexts = [
+    'ORDER A RIDE ',
+    'SELECT A CAR',
+    'RIDE IN STYLE',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: OnBoardingSlider(
-        headerBackgroundColor: Colors.transparent,
-        controllerColor: Colors.deepOrange,
-        finishButtonText: 'Create Account',
-        finishButtonStyle: const FinishButtonStyle(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          backgroundColor: Colors.deepOrange,
-        ),
-        background: [
-          Image.asset('assets/images/Slider1.png'),
-          Image.asset('assets/images/Slider2.png'),
-          Image.asset('assets/images/Slider3.png'),
-        ],
-        totalPage: 3,
-        speed: 1.8,
-        pageBodies: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 480,
-                ),
-                Text(
-                  'Description Text 1',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 480,
-                ),
-                Text(
-                  'Description Text 2',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 480,
-                ),
-                Text('Description Text 3',
-                    style: TextStyle(color: Colors.white)),
-              ],
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Expanded(
+            child: CarouselSlider.builder(
+              options: CarouselOptions(
+                enableInfiniteScroll: false,
+                height: MediaQuery.of(context).size.height,
+                viewportFraction: 1.0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                aspectRatio: MediaQuery.of(context).size.aspectRatio,
+                enlargeCenterPage: true,
+                autoPlay: false,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                pauseAutoPlayOnTouch: true,
+                scrollDirection: Axis.horizontal,
+                scrollPhysics: const BouncingScrollPhysics(),
+              ),
+              itemCount: 3,
+              itemBuilder: (BuildContext context, int index, int realIndex) {
+                return Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/Slider${index + 1}.png'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        imageTexts[index], // Display corresponding text
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    // SizedBox(
+                    //   height: 20.0,
+                    // ),
+                    // Text(
+                    //   secondTexts[index], // Display corresponding text
+                    //   style: TextStyle(
+                    //     color: Colors.white,
+                    //     fontSize: 18.0,
+                    //     fontWeight: FontWeight.bold,
+                    //   ),
+                    // ),
+                  ],
+                );
+              },
             ),
           ),
         ],
