37408526b72e711bb01276faa71c31a93b4fc39f
Musavir <88766824+Musavir-Ali@users.noreply.github.com>
2024-04-29 02:32:35 +0500

    Added files

--- lib/License.dart
+++ lib/License.dart
@@ -104,9 +104,13 @@ class License extends StatelessWidget {
               children: [
                 Container(
                   width: MediaQuery.of(context).size.width * 0.9,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                   child: const ListTile(
                     leading: Icon(
-                      Ionicons.document_attach_outline,
+                      Ionicons.camera_outline,
                       color: Colors.green,
                     ),
                     title: Text(
@@ -117,53 +121,192 @@ class License extends StatelessWidget {
                           fontWeight: FontWeight.bold),
                     ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[900],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
                 )
               ],
             ),
-            const SizedBox(height: 15),
+            const SizedBox(
+              height: 30,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "OR",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
                   width: MediaQuery.of(context).size.width * 0.9,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text("Name",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 12,
-                          )),
-                      SizedBox(
-                        height: 30,
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'First Name',
-                            hintStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12,
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
                   decoration: BoxDecoration(
                     color: Colors.grey[900],
                     borderRadius: BorderRadius.circular(10),
                   ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 20.0),
+                        child: Text("Full Name",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 30,
+                        width: MediaQuery.of(context).size.width * 0.85,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 20.0),
+                        child: Text("License ID",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 30,
+                        width: MediaQuery.of(context).size.width * 0.85,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Text("Date of Birth",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                  )),
+                              SizedBox(
+                                height: 30,
+                                width: MediaQuery.of(context).size.width * 0.3,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 50,
+                          ),
+                          Column(
+                            children: [
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Text("Expiration Date",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                  )),
+                              SizedBox(
+                                height: 30,
+                                width: MediaQuery.of(context).size.width * 0.3,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      )
+                    ],
+                  ),
                 )
               ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.19,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 40.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) => //(),
+                    //   ),
+                    // );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
