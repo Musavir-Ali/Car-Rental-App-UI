bbe60d67d9d9a74ae06a4be888562f0eec5a8bb9
Musavir <88766824+Musavir-Ali@users.noreply.github.com>
2024-05-01 02:13:02 +0500

    Added files

--- lib/Home.dart
+++ lib/Home.dart
@@ -5,7 +5,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color.fromRGBO(108, 84, 145, 100),
       body: SafeArea(
           child: Column(
@@ -19,8 +19,50 @@ class Home extends StatelessWidget {
                     color: Colors.black,
                     fontWeight: FontWeight.bold)),
           ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SearchBox(),
+          ),
         ],
       )),
+    );
+  }
+}
+
+class SearchBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(
+        hintText: "Search any car...",
+        hintStyle: TextStyle(
+          color: Colors.black,
+        ),
+        prefixIcon: Icon(Icons.search,
+            color: Colors.black), // Optional: Adds a search icon
+        suffixIcon: Icon(
+          Icons.filter_list,
+          color: Colors.black,
+        ), // Optional: Adds a filter icon
+        filled: true, // Optional: Makes the background white or filled
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)), // Round corners
+          borderSide: BorderSide(
+            color: Colors.black,
+            width: 1, // Border width
+          ),
+        ),
+        // This removes the default blue border on focus
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          borderSide: BorderSide(
+            color: Colors.black,
+            width: 2, // Width when focused
+          ),
+        ),
+      ),
     );
   }
 }
