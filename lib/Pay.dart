091b4d6b344838c73d43b7068374165a7034dcc8
Musavir <88766824+Musavir-Ali@users.noreply.github.com>
2024-05-09 04:08:54 +0500

    In Development

--- lib/Pay.dart
+++ lib/Pay.dart
@@ -33,16 +33,84 @@ class _PayState extends State<Pay> {
           ),
         ),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         padding: EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Payment Options",
+            const Text("Payment Options",
                 style: TextStyle(
                     fontSize: 14,
                     color: Colors.white,
                     fontWeight: FontWeight.bold)),
+            const SizedBox(height: 5),
+            Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: Image.asset('assets/images/MasterCard.png'),
+                      title: const Text("**** **** **** 5678",
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                      trailing: const Icon(Icons.arrow_drop_down_sharp,
+                          color: Colors.white, size: 25),
+                    ),
+                    const Divider(color: Colors.grey),
+                    const ListTile(
+                      leading: Icon(Icons.wallet, color: Colors.white),
+                      title: Text("Credit/Debit Card",
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                      trailing: Icon(Icons.arrow_drop_up_sharp,
+                          color: Colors.white, size: 25),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const SizedBox(width: 5),
+                              Image.asset(
+                                "assets/images/MasterCard.png",
+                              ),
+                              const SizedBox(width: 5),
+                              Image.asset("assets/images/Amix.png"),
+                              const SizedBox(width: 5),
+                              Image.asset("assets/images/Visa.png"),
+                              const SizedBox(width: 5),
+                              Image.asset("assets/images/Rupay.png"),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: "Card Number",
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )),
           ],
         ),
       ),
