99175a772bed19aa4244670eba6e871c8be99049
Musavir <88766824+Musavir-Ali@users.noreply.github.com>
2024-05-09 03:14:20 +0500

    Added files

--- lib/Booking.dart
+++ lib/Booking.dart
@@ -43,212 +43,246 @@ class _BookingState extends State<Booking> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0), // Consistent padding
-        child: Column(
-          crossAxisAlignment:
-              CrossAxisAlignment.start, // Align elements to the start
-          children: [
-            const SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Make the image take a reasonable fraction of the screen width
-                Container(
-                  width: screenWidth * 0.4,
-                  height: 100,
-                  child: Image.asset("assets/images/BMW840i.png",
-                      fit: BoxFit.cover, width: double.infinity),
-                ),
-                const SizedBox(width: 16), // Space between image and text
-                // Wrap the text in Flexible to prevent overflow on smaller screens
-                const Flexible(
-                  child: Column(
-                    crossAxisAlignment:
-                        CrossAxisAlignment.start, // Align text to start
-                    children: [
-                      Text(
-                        'BMW 840i',
-                        style: TextStyle(
-                          color: Colors.deepOrange,
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text("145 Trips",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          )),
-                      SizedBox(height: 5),
-                      Text("\$ 200/Hr",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          )),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            const Text("Trip Details",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                )),
-            const SizedBox(height: 15),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.13,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.grey[900],
-              ),
-              child: Center(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      _formatDate(
-                          selectedDates.isNotEmpty ? selectedDates[0] : null),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    const Icon(
-                      Icons.arrow_forward,
-                      color: Colors.deepOrange,
-                    ),
-                    const SizedBox(width: 10),
-                    Text(
-                      _formatDate(
-                          selectedDates.length > 1 ? selectedDates[1] : null),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text("Sub Total",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                )),
-            const SizedBox(height: 5),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey[900],
-              ),
-              child: const Column(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0), // Consistent padding
+          child: Column(
+            crossAxisAlignment:
+                CrossAxisAlignment.start, // Align elements to the start
+            children: [
+              const SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Row(
+                  // Make the image take a reasonable fraction of the screen width
+                  Container(
+                    width: screenWidth * 0.4,
+                    height: 100,
+                    child: Image.asset("assets/images/BMW840i.png",
+                        fit: BoxFit.cover, width: double.infinity),
+                  ),
+                  const SizedBox(width: 16), // Space between image and text
+                  // Wrap the text in Flexible to prevent overflow on smaller screens
+                  const Flexible(
+                    child: Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.start, // Align text to start
                       children: [
                         Text(
-                          "\$ 200 x 7 Days",
+                          'BMW 840i',
                           style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
+                            color: Colors.deepOrange,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        const Spacer(),
-                        Text(
-                          "\$33600",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "Trip Fee",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
-                        const Spacer(),
-                        Text(
-                          "\$300",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "Discount",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
-                        const Spacer(),
-                        Text(
-                          "- -",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.deepOrange,
-                    thickness: 1,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "Total Amount",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
-                        const Spacer(),
-                        Text(
-                          "\$33900",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
+                        SizedBox(height: 5),
+                        Text("145 Trips",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            )),
+                        SizedBox(height: 5),
+                        Text("\$ 200/Hr",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            )),
                       ],
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+              const Text("Trip Details",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const SizedBox(height: 15),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.13,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey[900],
+                ),
+                child: Center(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        _formatDate(
+                            selectedDates.isNotEmpty ? selectedDates[0] : null),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const Icon(
+                        Icons.arrow_forward,
+                        color: Colors.deepOrange,
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        _formatDate(
+                            selectedDates.length > 1 ? selectedDates[1] : null),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text("Sub Total",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const SizedBox(height: 5),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey[900],
+                ),
+                child: const Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "\$ 200 x 7 Days",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const Spacer(),
+                          Text(
+                            "\$33600",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Trip Fee",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const Spacer(),
+                          Text(
+                            "\$300",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Discount",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const Spacer(),
+                          Text(
+                            "- -",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.deepOrange,
+                      thickness: 1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Total Amount",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                          Spacer(),
+                          Text(
+                            "\$33900",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.14),
+              SizedBox(
+                width: double.infinity,
+                height: 40.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) => const (),
+                    //   ),
+                    // );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "Proceed to Pay",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              )
+            ],
+          ),
         ),
       ),
     );
