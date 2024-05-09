327ac1ef96c151024b0fff8e5e218649e5397caf
Musavir <88766824+Musavir-Ali@users.noreply.github.com>
2024-05-09 23:30:21 +0500

    added files

--- lib/Pay.dart
+++ lib/Pay.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -46,57 +47,61 @@ class _PayState extends State<Pay> {
                     fontWeight: FontWeight.bold)),
             const SizedBox(height: 5),
             Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[900],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: Image.asset('assets/images/MasterCard.png'),
-                      title: const Text("**** **** **** 5678",
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold)),
-                      trailing: const Icon(Icons.arrow_drop_down_sharp,
-                          color: Colors.white, size: 25),
+              decoration: BoxDecoration(
+                color: Colors.grey[900],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Image.asset('assets/images/MasterCard.png'),
+                    title: const Text("**** **** **** 5678",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold)),
+                    trailing: const Icon(Icons.arrow_drop_down_sharp,
+                        color: Colors.white, size: 25),
+                  ),
+                  const Divider(color: Colors.grey),
+                  const ListTile(
+                    leading: Icon(Icons.wallet, color: Colors.white),
+                    title: Text("Credit/Debit Card",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold)),
+                    trailing: Icon(Icons.arrow_drop_up_sharp,
+                        color: Colors.white, size: 25),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    const Divider(color: Colors.grey),
-                    const ListTile(
-                      leading: Icon(Icons.wallet, color: Colors.white),
-                      title: Text("Credit/Debit Card",
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold)),
-                      trailing: Icon(Icons.arrow_drop_up_sharp,
-                          color: Colors.white, size: 25),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.85,
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              const SizedBox(width: 5),
-                              Image.asset(
-                                "assets/images/MasterCard.png",
-                              ),
-                              const SizedBox(width: 5),
-                              Image.asset("assets/images/Amix.png"),
-                              const SizedBox(width: 5),
-                              Image.asset("assets/images/Visa.png"),
-                              const SizedBox(width: 5),
-                              Image.asset("assets/images/Rupay.png"),
-                            ],
-                          ),
-                          const SizedBox(height: 10),
-                          const TextField(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const SizedBox(width: 10),
+                            Image.asset(
+                              "assets/images/MasterCard.png",
+                            ),
+                            const SizedBox(width: 10),
+                            Image.asset("assets/images/Amix.png"),
+                            const SizedBox(width: 10),
+                            Image.asset("assets/images/Visa.png"),
+                            const SizedBox(width: 10),
+                            Image.asset("assets/images/Rupay.png"),
+                            const SizedBox(width: 10),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: const TextField(
                             decoration: InputDecoration(
                               labelText: 'Card Number',
                               labelStyle: TextStyle(
@@ -110,8 +115,11 @@ class _PayState extends State<Pay> {
                             ),
                             keyboardType: TextInputType.number,
                           ),
-                          const SizedBox(height: 20.0),
-                          const Row(
+                        ),
+                        const SizedBox(height: 20.0),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: const Row(
                             children: [
                               Flexible(
                                 child: TextField(
@@ -149,11 +157,25 @@ class _PayState extends State<Pay> {
                               ),
                             ],
                           ),
-                        ],
-                      ),
-                    )
-                  ],
-                )),
+                        ),
+                        const SizedBox(height: 10.0),
+                      ],
+                    ),
+                  ),
+                  const Divider(color: Colors.grey),
+                  ListTile(
+                    leading: Image.asset('assets/images/GooglePay.png'),
+                    title: const Text("Apple Pay",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold)),
+                    trailing: const Icon(Icons.arrow_drop_down_sharp,
+                        color: Colors.white, size: 25),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
